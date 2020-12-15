3eedd4dd7ad5dfdcbeebabce8a25a36e07b17448
Johan Castillo <jcjohan2707@gmail.com>
2020-12-15 11:23:49 -0400

    Structured basic HTML firts navbar

---
+++ src/components/navbar-mobile/NavbarMobile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NavbarMobile = () => (
+    <>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="">Enlace 1</a></li>
+                    <li><a href="">Enlace 2</a></li>
+                    <li><a href="">Enlace 3</a></li>
+                    <li><a href="">Enlace 4</a></li>
+                    <li><a href="">Enlace 5</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+);
+
+export default NavbarMobile
